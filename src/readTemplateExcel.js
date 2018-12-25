b8f205156f5c8d2939b564c07f85d6f8212cdddb
ejiu <blackfan@163.com>
2018-12-25 21:30:35 +0800

    切换版本

--- src/readTemplateExcel.js
+++ src/readTemplateExcel.js
@@ -6,21 +6,17 @@ const xlsx = require('node-xlsx');
 //custom module
 const config = require('./config.js');
 
-const sheets = xlsx.parse(config.templateExcelPath);//获取到所有sheets
-const coursesName = sheets[0]['data'][0][0];        //第一张表 第一列 第一行 为课程名称
-let pageInfo = [];                                  //各页面模版信息
+const sheets = xlsx.parse(config.templateExcelPath);        //获取到所有sheets
+let pageMap = new Map();                                    //各页面模版信息
 
 sheets.forEach(function(sheet){
     for(let rowId in sheet['data']){
         let row=sheet['data'][rowId];
-        pageInfo.push(row[1]);
+        pageMap.set(row[0], row[1]);                        //模版英文名 : 模版名
     }
 });
 
-pageInfo.shift();
-pageInfo.shift();
 
 module.exports = {
-    coursesName : coursesName,
-    pageInfo : pageInfo,
+    pageMap : pageMap,
 };
